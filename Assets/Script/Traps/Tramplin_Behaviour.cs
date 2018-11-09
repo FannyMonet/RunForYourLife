61382e4e7b315e783ec60e2bcd73a3d259e22934
Paul Giovannini <paul.giovannini96@gmail.com>
2018-11-09 17:41:05 -0500

    Piege de saut, ok

---
+++ Assets/Script/Traps/Tramplin_Behaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tramplin_Behaviour : MonoBehaviour {
+
+
+    public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D (Collider2D col)
+	{
+		if (col.CompareTag ("Player")) {
+			anim.SetTrigger ("TramplinActif");
+			col.GetComponent<Movement >().aRgbd.velocity = new Vector2 (col.GetComponent<Movement >().aRgbd.velocity.x, 150);
+		}
+	}
+}
