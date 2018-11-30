6a6d089b55c68c04a5bb7ec113265cca40c6fe9f
Paul Giovannini <paul.giovannini96@gmail.com>
2018-11-30 13:36:05 -0500

    30/11/2108

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -6,14 +6,28 @@ public class SoundManager : MonoBehaviour {
 
     public static SoundManager Instance;
 
+    public AudioClip[] clip;
+
+    public AudioSource S1;
+	public AudioSource S2;
+	public AudioSource S3;
+	public AudioSource S4;
+
+
 	// Use this for initialization
 	void Awake ()
 	{
 	    
 		if (Instance == null) {
 
-		Instance = this;
+		    Instance = this;
+		    S1 = GameObject.Find("S1").GetComponent<AudioSource>();
+			S2 = GameObject.Find("S2").GetComponent<AudioSource>();
+			S3 = GameObject.Find("S3").GetComponent<AudioSource>();
+			S4 = GameObject.Find("S4").GetComponent<AudioSource>();
+
 			DontDestroyOnLoad(Instance.gameObject);
+
 
 		}
 		else if(Instance!=this)
